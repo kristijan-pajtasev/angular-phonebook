85c7065b59e458fa9b0768e8b565949b172a7cce
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-04-30 16:19:51 +0100

    service implementation of get and getAll

--- public/src/phonebook/phonebook.service.js
+++ public/src/phonebook/phonebook.service.js
@@ -1,19 +1,24 @@
+let $http;
 class PhonebookService {
-    constructor() {
-
+    constructor(_$http_) {
+        $http = _$http_;
     }
 
     getAll(res, rej) {
-        return new Promise(() => {
-            res([]);
-        })
+        $http.get("mocks/phonebook.json").then((data) => {
+            res(data.data);
+        });
     }
 
     get(id, res, rej) {
-        return new Promise(() => {
-            res({});
-        })
+        $http.get("mocks/phonebook.json").then((data) => {
+            let contact = data.data.filter(e => e.id === id)[0];
+            if(!contact) rej(`Contact with ${id} does not exist.`);
+            else res(contact);
+        });
     }
 }
+
+PhonebookService.$inject = ["$http"];
 
 angular.module("phonebook").service("PhonebookService", PhonebookService);
