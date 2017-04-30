6694a3b6eae73f27389cec1a774d899b41e4f60a
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2017-04-30 16:02:17 +0100

    phonebook service with service signature

--- public/src/phonebook/phonebook.service.js
+++ public/src/phonebook/phonebook.service.js
@@ -2,6 +2,18 @@ class PhonebookService {
     constructor() {
 
     }
+
+    getAll(res, rej) {
+        return new Promise(() => {
+            res([]);
+        })
+    }
+
+    get(id, res, rej) {
+        return new Promise(() => {
+            res({});
+        })
+    }
 }
 
 angular.module("Phonebook").service("PhonebookService", PhonebookService);
