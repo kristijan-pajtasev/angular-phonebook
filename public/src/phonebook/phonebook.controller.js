e38d2d485208920f543ae0a2e73f0090a6c2f504
Mislav Novakovic <mislav.novakovic@gmail.com>
2017-05-01 17:05:06 +0200

    ADD phonebook post in controller
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- public/src/phonebook/phonebook.controller.js
+++ public/src/phonebook/phonebook.controller.js
@@ -1,5 +1,20 @@
-angular.module('phonebook').controller("PhonebookController", ["PhonebookService", "$scope", function(PhonebookService, $scope) {
+angular.module('phonebook').controller("PhonebookController", ["PhonebookService", "$scope", "$http", function(PhonebookService, $scope, $http) {
     PhonebookService.getAll(jsonData => {
         $scope.contacts = jsonData;
     });
+
+    $scope.postContact = function (firstName, lastName, phoneNumber) {
+    var data = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber
+    };
+
+    $http.post('/phonebook', JSON.stringify(data)).then(function (response) {
+        if (response.data)
+            $scope.msg = "Post Data Submitted Successfully!";
+        }, function (response) {
+            $scope.msg = "Service does not exist";
+        });
+    };
 }]);
